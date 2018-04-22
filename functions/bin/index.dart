67b82afc425f82d7f2eb95d62d32387364e4995b
Charles Cagle <cjcaglejr@gatech.edu>
2018-04-22 19:53:05 +0000

    make main() not async

--- functions/bin/index.dart
+++ functions/bin/index.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_functions_interop/firebase_functions_interop.dart';
 import 'package:myproject_functions/data_access/dpgs.dart';
 
-void main() async {
+void main() {
   var httpsFunc = firebaseFunctions.https.onRequest((request, response) {
     dpgsUpdateTournamentSchedules().then((_) {
       response.send('Hello from Firebase Functions Dart Interop!');
